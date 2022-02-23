ce26a00f6613f566cb368ff3d881b382f0249631
Thomas <65050898+tvonment@users.noreply.github.com>
2022-02-23 15:37:43 +0000

    insert content

--- src/client/contestResultsTab/ContestResultsTab.tsx
+++ src/client/contestResultsTab/ContestResultsTab.tsx
@@ -3,7 +3,25 @@ import { Provider, Flex, Text, Button, Header } from "@fluentui/react-northstar"
 import { useState, useEffect } from "react";
 import { useTeams } from "msteams-react-base-component";
 import * as microsoftTeams from "@microsoft/teams-js";
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 /**
  * Implementation of the Contest Results content page
@@ -15,32 +33,15 @@ export const ContestResultsTab = () => {
 
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [contestresults, setResult] = useState({});
+    const [contestresults, setResult] = useState([]);
 
     useEffect(() => {
-        if (inTeams === true) {
-            microsoftTeams.appInitialization.notifySuccess();
-        } else {
-            setEntityId("Not in Microsoft Teams");
-        }
-    }, [inTeams]);
-
-    useEffect(() => {
-        if (context) {
-            setEntityId(context.entityId);
-        }
-    }, [context]);
-
-
-    useEffect(() => {
-        fetch("https://kulti22.azurewebsites.net/api/GetBandCompetitionVotes?code=" + process.env.FUNC_AUTH_CODE)
+        fetch("https://kulti22.azurewebsites.net/api/GetBandCompetitionVotes?code=zWMAc4ksGCGmQDin1O2d1BTvx4Y0jz2Q23vWByWFAEQl7zMNA39Hyg==")
             .then(res => res.json())
             .then(
                 (result) => {
                     setIsLoaded(true);
                     setResult(result);
-                    console.log(result);
-
                 },
                 // Note: it's important to handle errors here
                 // instead of a catch() block so that we don't swallow
@@ -52,32 +53,60 @@ export const ContestResultsTab = () => {
             )
     }, []);
 
+    const options = {
+        indexAxis: 'y' as const,
+        elements: {
+            bar: {
+                borderWidth: 3,
+            },
+        },
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            },
+            title: {
+                display: false,
+            },
+        },
+    };
+
+    const labels = contestresults!.map((item: any) => item.name);
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                data: contestresults!.map((item: any) => item.count),
+                borderColor: 'rgb(240,113,0)',
+                backgroundColor: 'rgba(240,113,0, 0.7)',
+            }
+        ],
+    };
+
     /**
      * The render() method to create the UI of the tab
      */
     return (
         <Provider theme={theme}>
-            <Flex fill={true} column styles={{
-                padding: ".8rem 0 .8rem .5rem"
+            <Flex fill={true} column hAlign="center" vAlign="center" styles={{
+                padding: ".8rem 0 .8rem .5rem", backgroundColor: "transparent"
             }}>
                 <Flex.Item>
-                    <Header content="This is your tab" />
+                    <Header content="Act Competition Results" />
                 </Flex.Item>
                 <Flex.Item>
                     <div>
                         <div>
                             <Text content={entityId} />
                         </div>
-
-                        <div>
-                            <Button onClick={() => alert("It worked!")}>A sample button</Button>
-                        </div>
+                        <Bar options={options} data={data} />;
                     </div>
                 </Flex.Item>
                 <Flex.Item styles={{
                     padding: ".8rem 0 .8rem .5rem"
                 }}>
-                    <Text size="smaller" content="(C) Copyright Jublaost" />
+                    <Text size="smaller" content="© Copyright Jublaost - Developed by Nilda" />
                 </Flex.Item>
             </Flex>
         </Provider>
